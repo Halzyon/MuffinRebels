d3911deecdadf3ef14c346597bac79ac72004c6b
Halzyon <112913743+Halzyon@users.noreply.github.com>
2022-10-28 18:50:15 +0800

    SpriteAnimation and Gameobject

---
+++ Inc/gameObject.h
@@ -0,0 +1,21 @@
+#ifndef H_GAMEOBJECT_H
+#define H_GAMEOBJECT_H
+#include "cprocessing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+	CP_Vector position;
+	CP_Vector direction;
+	CP_Vector scale;
+	float rotation;
+	float speed;
+}GameObject;
+
+
+
+
+
+
+#endif
