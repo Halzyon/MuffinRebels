9fec1eae89cd91a74312311729db8665f2ad81b3
Foong Jun Wei <fjunwei178@gmail.com>
2022-10-28 15:56:15 +0800

    meme

--- Src/main.c
+++ Src/main.c
@@ -1,34 +1,49 @@
-#include "cprocessing.h"
-#include "UI/DiceUI/dice_ui.h"
+//#include "cprocessing.h"
+#include "FileIO/fileIO.h"
+#include "FileIO/serialization.h"
+#include "FileIO/encode.h"
+#include "Character/gameMap.h"
+#include "Character/gameChar.h"
 #include "Character/diceHandler.h"
-#include <stdio.h>
+#include "Character/charMovement.h"    
 
 CP_Image logo;
+game_map* map1;
 
 void game_init(void)
 {
-	dice_ui_init();
+	logo = CP_Image_Load("Assets/DigiPen_Singapore_WEB_RED.png");
+	CP_Settings_ImageMode(CP_POSITION_CORNER);
+	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
+
+	CP_System_SetWindowSize(CP_Image_GetWidth(logo), CP_Image_GetHeight(logo));
+
 	init_dice();
-	CP_System_Fullscreen();
+	init_char(Warrior);
+	init_map_obj(10, 10, &map1);
 }
-
-
 
 void game_update(void)
 {
-	CP_Graphics_ClearBackground(CP_Color_Create(0,0,0,255));
-
-	if (CP_Input_KeyDown(KEY_ESCAPE))
+	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
+	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), 255);
+	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
 	}
+
+	hardware_handler();
+
 }
 
 void game_exit(void)
 {
-	dice_shutdown();
 	CP_Image_Free(&logo);
+	free_map_obj(&map1);
+	free_char();
 }
+
+
 
 
 int main(void)
@@ -36,5 +51,6 @@ int main(void)
 	//CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);
 	CP_Engine_SetNextGameState(game_init, game_update, game_exit);
 	CP_Engine_Run();
+
 	return 0;
 }
