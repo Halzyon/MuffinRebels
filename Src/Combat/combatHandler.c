40a485108a712cf0f6beb698ebfec7b5383fa060
AlwinMoo <geraldcheu@gmail.com>
2022-11-07 14:50:44 +0800

    broken code apparently so let's make it stale

--- Src/Combat/combatHandler.c
+++ Src/Combat/combatHandler.c
@@ -1,1 +1,18 @@
 #include "combatHandler.h"
+#include "../Character/diceHandler.h"
+
+void declare_combatants(GameObject* const priority, GameObject* const defendant)
+{
+	priority_go = &priority;
+	defending_go = &defendant;
+	combatants_present = 1;
+}
+
+int combat_phase(void)
+{
+	//if combatants have not been set and/or you have changed combatants, always init;
+	if (!combatants_present)
+		return 1;
+
+	return 0;
+}
--- Src/Combat/combatHandler.h
+++ Src/Combat/combatHandler.h
@@ -1,6 +1,16 @@
 #ifndef combat_handler_h
 #define combat_handler_h
+#include "../GameObject/gameObject.h"
 
+* priority_go; //priority will always be counted as attacking while the combat while defend
+
+GameObject* defending_go;
+
+int combatants_present;
+
+void declare_combatants(GameObject* const priority, GameObject* const defendant);
+
+int combat_phase(void);
 
 
 #endif // !combat_handler_h
