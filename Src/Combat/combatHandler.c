a952b04dd80418cb1fbebb967a3e6b01f1399597
AlwinMoo <geraldcheu@gmail.com>
2022-11-07 15:16:55 +0800

    added pseudo code to make starting easier

--- Src/Combat/combatHandler.c
+++ Src/Combat/combatHandler.c
@@ -5,6 +5,7 @@ void declare_combatants(GameObject* const priority, GameObject* const defendant)
 {
 	priority_go = &priority;
 	defending_go = &defendant;
+	
 	combatants_present = 1;
 }
 
@@ -14,5 +15,12 @@ int combat_phase(void)
 	if (!combatants_present)
 		return 1;
 
+	// attackers roll for max dice
+	// roll + sum
+	// defenders take total number of dice - 1
+	// roll + sum
+	// decrease health of difference of whoever is lower
+	
+	combatants_present = 0;
 	return 0;
 }
