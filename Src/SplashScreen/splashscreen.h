7b943d6aee3d86bbf47dcba60523c7e17af811f4
Halzyon <112913743+Halzyon@users.noreply.github.com>
2022-11-07 18:46:49 +0800

    added GamestateManager

---
+++ Src/SplashScreen/splashscreen.h
@@ -0,0 +1,14 @@
+#ifndef H_SPLASHSCREEN_H
+#define H_SPLASHSCREEN_H
+
+void splash_screen_init(void);
+void splash_screen_update(void);
+void splash_screen_exit(void);
+
+#endif // !H_SPLASHSCREEN_H
+
+
+
+
+
+
--- Src/main.c
+++ Src/main.c
@@ -1,48 +1,6 @@
-#include "SpriteAnimation/spriteAnimation.h"
-
-CP_Image logo;
-Sprite* ash;
-void game_init(void)
-{
-	logo = CP_Image_Load("Assets/DigiPen_Singapore_WEB_RED.png");
-	
-	ash = CreateSprite("Assets/poke.png",4,4,true,true);
-
-	CP_Settings_ImageMode(CP_POSITION_CORNER);
-	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
-	
-	CP_System_SetWindowSize(CP_Image_GetWidth(logo), CP_Image_GetHeight(logo) * 3);
-}
-
-void game_update(void)
-{
-	UpdateSprite(ash, CP_System_GetDt());
-	CP_Graphics_ClearBackground(CP_Color_Create(0,0,0,255));
-
-	//CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), 255);
-	RenderSprite(ash);
-
-	if (CP_Input_KeyDown(KEY_Q))
-	{
-		CP_Engine_Terminate();
-	}
-	
-}
-
-void game_exit(void)
-{
-	CP_Image_Free(&logo);
-	CP_Image_Free(&ash->go.image);
-	free(ash);
-}
-
-
-
+#include "GameStateManager.h"
 
 void main(void)
 {
-	//CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);
-	CP_Engine_SetNextGameState(game_init, game_update, game_exit);
-	CP_Engine_Run();
-	return 0;
+	GameStateRun();
 }
