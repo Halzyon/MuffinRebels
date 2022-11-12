878cace0bddf500dd6cd31b1a9952aa3ce9e443a
yoyijia <112854194+yoyijia@users.noreply.github.com>
2022-11-12 19:23:30 +0800

    update

--- Src/yijia/overworld_ui.c
+++ Src/yijia/overworld_ui.c
@@ -32,20 +32,19 @@ void overworld_init(void) {
 	settings_icon.position.y = 50;
 
 	button.position.x = CP_System_GetWindowWidth() - 640;
-	button.position.y = 240;
+	button.position.y = 220;
 
 	buttonpressed.position.x = CP_System_GetWindowWidth() - 640;
 	buttonpressed.position.y = 240;
 
-
 	lvl1icon.position.x = CP_System_GetWindowWidth() - 640;
-	lvl1icon.position.y = 250;
+	lvl1icon.position.y = 220;
 
-	lvl2icon.position.x = CP_System_GetWindowWidth() - 640;
-	lvl2icon.position.y = 360;
+	lvl2icon.position.x = CP_System_GetWindowWidth() - 640; 
+	lvl2icon.position.y = 370;
 
 	lvl3icon.position.x = CP_System_GetWindowWidth() - 640;
-	lvl3icon.position.y = 460;
+	lvl3icon.position.y = 520;
 
 }
 
@@ -53,18 +52,28 @@ void overworld_init(void) {
 void overworld_update(void) {
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
 
-	RenderAsset(settings_icon, 255);
-	RenderAsset(button, 255);
-	
-	/*for (int i = 0; i < 3; i++)
-	{
-		CP_Image_Draw(button.image, button.position.y + (i * 100), button.position.x, 396, 93, 255);
-	}*/
+	RenderAsset(settings_icon, 255); 
 
-	RenderAsset(lvl1icon, 255);
-	RenderAsset(lvl2icon, 255);
-	RenderAsset(lvl3icon, 255);
-}
+	for (int i = 0; i < 3; i++)
+	{
+		CP_Image_Draw(button[0].image, button[0].position.x , button[0].position.y + (i * 150), 396, 93, 255);
+
+		CP_Image_Draw(lvl1icon.image,lvl1icon.position.x, lvl1icon.position.y,146,32,255);
+
+		if (mouse_in_rect(button.position.x, button.position.y + (i * 150), 396,93) == 1 && CP_Input_MouseClicked()) {
+			button.clicked[i] = !button.clicked[i];
+
+		}
+		RenderAsset(buttonpressed);
+		if (mouse_in_rect(button.position.x, button.position.y + (i * 150), 396, 93) == 1 && CP_Input_MouseClicked()) 	
+		{
+			button.clicked = !button.clicked;
+		}
+		RenderAsset(lvl2icon, 255);
+		RenderAsset(lvl3icon, 255);
+	}
+
+	}
 
 
 void overworld_shutdown(void) {
